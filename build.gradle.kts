fe44ac5dcc7ddc12b18e2d45059265e90fc87ced
BizCub <bizcubmine@gmail.com>
2026-08-11 09:36:39 +0200

    Update ml plugin to version 0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("me.modmuss50.mod-publish-plugin")
-    id("dev.kikugie.fletching-table")
-    id("com.bizcub.multiloader")
+    id("io.github.bizcub.multiloader")
 }
 
 multiloader {
@@ -18,6 +16,13 @@ multiloader {
     setMREnvironment(mrEnvs.clientOnly)
     setCFEnvironment(cfEnvs.client)
 
+    versionRange("26.1.2", to = "latest")
+    versionRange("1.21.3", to = "1.21.11")
+    versionRange("1.21.1", to = "1.20.5", loader = "fabric")
+    versionRange("1.21.1", to = "1.20.6", loader = "forge")
+    versionRange("1.20.2", to = "latest")
+    versionRange("1.20.2", to = "1.20.4", loader = "forge")
+
     addDependency(
         dependency = "me.shedaniel.cloth:cloth-config-${mod.loader}:${getDep("cloth-config").split("+").first()}",
         configuration = if (isClothConfigAvailable) "implementation" else "compileOnly",
@@ -26,7 +31,7 @@ multiloader {
         isPublishDepEnabled = isClothConfigAvailable,
         publishProjectId = "cloth-config"
     )
-    if (!(scp eq "1.19")) addDependency(
+    addDependency(
         dependency = "maven.modrinth:enhanced-world-creation:${getDep("enhanced-world-creation")}",
         configuration = "runtimeOnly"
     )
