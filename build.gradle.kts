ec8bcb3ec508c240f44f0436e96dd8f6056b6ea4
BizCub <bizcubmine@gmail.com>
2026-08-12 16:57:00 +0200

    Correct versions for publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,8 @@ multiloader {
 
     versionRange("26.1.2", to = "latest")
     versionRange("1.21.3", to = "1.21.11")
-    versionRange("1.21.1", to = "1.20.5", loader = "fabric")
-    versionRange("1.21.1", to = "1.20.6", loader = "forge")
-    versionRange("1.20.2", to = "latest")
+    versionRange("1.21.1", from = "1.20.6", loader = "forge")
+    versionRange("1.20.2", to = "1.21.1")
     versionRange("1.20.2", to = "1.20.4", loader = "forge")
 
     addDependency(
