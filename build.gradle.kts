c6886ab4bbd57111724676fae841f8d38b334fbc
BizCub <bizcubmine@gmail.com>
2026-08-28 13:11:16 +0200

    Update Simple Config Lib dependency to 1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 multiloader {
     sc.replacements {
         string(scp >= "1.21.11" && !isForge, "auto_config") {
-            replace("AutoConfig", "AutoConfigClient")
+            replace("me.shedaniel.autoconfig.AutoConfig", "me.shedaniel.autoconfig.AutoConfigClient")
+            replace("AutoConfig.getConfigScreen", "AutoConfigClient.getConfigScreen")
         }
     }
 
@@ -23,7 +24,7 @@ multiloader {
         configuration = "runtimeOnly"
     )
     addDependency(
-        dependency = "maven.modrinth:simple-config-lib:${getDep("simple-config-lib")}",
+        dependency = getSimpleConfigLibDep("1.1"),
         isPublishDepEnabled = true
     )
     val isClothConfigAvailable = !(isForge && scp > "1.21.3")
